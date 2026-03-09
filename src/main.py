5068448a4eae8e382ec2ccb4849acf901a7af9dd
Nadir Kaci <N.kaciaissa@2025.ljmu.ac.uk>
2026-03-09 12:02:01 +0000

    Started wokr on Main page

--- src/main.py
+++ src/main.py
@@ -1,6 +1,29 @@
-print("*** Ai Impact on jobs ***")
-print("1. Salary Analysis")
-print("2. Automation Risk Analysis")
-print("3. Growth vs Risk Comparision")
-print("4. Search by skill")
-print("5. Exit")
+test = True
+while test:
+
+    prompt = "*** Ai Impact on jobs ***"
+    prompt += "\n1. Salary Analysis"
+    prompt += "\n2. Automation Risk Analysis"
+    prompt += "\n3. Growth vs Risk Comparision"
+    prompt += "\n4. Search by skill"
+    prompt += "\n5. Exit"
+    prompt += "\n"
+
+    answer = input(prompt)
+    if answer == 1 :
+        print('Go To Salary Analysis Feature') 
+    
+    if answer == 2 :
+        print('Go To Automation Risk Analysis Feature') 
+
+    if answer == 3 :
+        print('Go To Growth vs Risk Analysis Feature') 
+
+    if answer == 4 :
+        print('Go To Search by skill Feature') 
+
+    if answer == 5 :
+        print('Exiting')
+        test = False
+
+    print(answer) 
