2ae59e8917d9ecc7573f447bede18a43cdb5b7a6
zainalsaleh <zainalsaleh34@gmail.com>
2026-04-15 16:41:04 +0100

    Update main_Nadir_Kaci.py

--- src/main_Nadir_Kaci.py
+++ src/main_Nadir_Kaci.py
@@ -45,9 +45,13 @@ def main_menu():
         elif choice == '6':
             jobs_by_keyword_menu()
         elif choice == '7':
+<<<<<<< Updated upstream
             run_data_summary()
         elif choice == '8':
             print("\nExiting program. Goodbye! 👋")
+=======
+            print("\nExiting program. Goodbye! ")
+>>>>>>> Stashed changes
             break
         else:
             print("\nInvalid input. Please enter a number from 1 to 8.\n")
